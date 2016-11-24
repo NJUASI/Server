266558e5c02fedef309f82741ba679a572e9dd92
Byron Dong <2104869365@qq.com>
2016-11-24 20:08:30 +0800

    因为user调整，修正po

--- src/main/java/po/WebManagerPO.java
+++ src/main/java/po/WebManagerPO.java
@@ -2,36 +2,34 @@ package po;
 
 import java.io.Serializable;
 
-public class WebManagerPO implements Serializable{
-	
+public class WebManagerPO implements Serializable {
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 3713445002016120118L;
 
-	//	管理人员编号
-	private String userID;
-	
-	//	密码
+	// 管理人员编号
+	private String webManagerID;
+
+	// 密码
 	private String password;
-	
+
 	public WebManagerPO(String userID, String password) {
-		super();
-		this.userID = userID;
+		this.webManagerID = userID;
 		this.password = password;
 	}
-	
-	
-	public String getUserID() {
-		return userID;
+
+	public String getWebManagerID() {
+		return webManagerID;
 	}
 
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 
 }
--- src/main/java/po/WebMarketerPO.java
+++ src/main/java/po/WebMarketerPO.java
@@ -2,32 +2,32 @@ package po;
 
 import java.io.Serializable;
 
-public class WebMarketerPO implements Serializable{
-	
+public class WebMarketerPO implements Serializable {
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -8577895472775494103L;
 
-	//	用户编号
-	private String userID;
-	
-	//	密码
+	// 用户编号
+	private String webMarketerID;
+
+	// 密码
 	private String password;
-	
+
 	public WebMarketerPO(String userID, String password) {
-		super();
-		this.userID = userID;
+		this.webMarketerID = userID;
 		this.password = password;
 	}
-	
-	public String getUserID() {
-		return userID;
+
+	public String getWebMarketerID() {
+		return webMarketerID;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
