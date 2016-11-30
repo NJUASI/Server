0148a387a35f57188a95994e123172d983b254dc
CharlesFeng <fdfengjunjie@126.com>
2016-11-30 23:48:47 +0800

    传掉了。。

--- src/main/java/dataHelper/DataFactory.java
+++ src/main/java/dataHelper/DataFactory.java
@@ -12,7 +12,66 @@ package dataHelper;
 public interface DataFactory {
 
 	/**
+	 * @author charles
+	 * @lastChangedBy charles
+	 * @updateTime 2016/11/30
+	 * @return 单例化的AddressDataHelper
 	 */
+	AddressDataHelper getAddressDataHelper();
 	
 	/**
+	 * @author charles
+	 * @lastChangedBy charles
+	 * @updateTime 2016/11/30
+	 * @return 单例化的CreditDataHelper
 	 */
+	CreditDataHelper getCreditDataHelper();
+	
+	/**
+	 * @author charles
+	 * @lastChangedBy charles
+	 * @updateTime 2016/11/30
+	 * @return 单例化的GuestDataHelper
+	 */
+	GuestDataHelper getGuestDataHelper();
+	
+	/**
+	 * @author charles
+	 * @lastChangedBy charles
+	 * @updateTime 2016/11/30
+	 * @return 单例化的HotelWorkerDataHelper
+	 */
+	HotelWorkerDataHelper getHotelWorkerDataHelper();
+	
+	/**
+	 * @author charles
+	 * @lastChangedBy charles
+	 * @updateTime 2016/11/30
+	 * @return 单例化的MarketDataHelper
+	 */
+	MarketDataHelper getMarketDataHelper();
+	
+	/**
+	 * @author charles
+	 * @lastChangedBy charles
+	 * @updateTime 2016/11/30
+	 * @return 单例化的OrderDataHelper
+	 */
+	OrderDataHelper getOrderDataHelper();
+	
+	/**
+	 * @author charles
+	 * @lastChangedBy charles
+	 * @updateTime 2016/11/30
+	 * @return 单例化的WebManagerDataHelper
+	 */
+	WebManagerDataHelper getWebManagerDataHelper();
+	
+	/**
+	 * @author charles
+	 * @lastChangedBy charles
+	 * @updateTime 2016/11/30
+	 * @return 单例化的WebMarketerDataHelper
+	 */
+	WebMarketerDataHelper getWebMarketerDataHelper();
+}
