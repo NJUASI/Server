84209f4ba22d5abe121c316b7569ba5a425fda12
CharlesFeng <fdfengjunjie@126.com>
2016-11-30 23:46:04 +0800

    新增dataFactory  单例实现

---
+++ src/main/java/dataHelper/DataFactory.java
@@ -0,0 +1,18 @@
+package dataHelper;
+
+/**
+ * 
+ * @author charles
+ * lastChangedBy charles
+ * updateTime 2016/11/30
+ * 
+ * 以工厂的方式得到每个DataHelper的实例
+ * Factory 单例化
+ */
+public interface DataFactory {
+
+	/**
+	 */
+	
+	/**
+	 */
